acb5b837d3ce2cdc371ca632d31a8f4c18026271
申建刚 <shenjiangang@thextrader.cn>
2018-12-04 16:46:32 +0800

    项目优化

--- buff-config/src/main/java/com/shenjg/config/ConfigServerApplication.java
+++ buff-config/src/main/java/com/shenjg/config/ConfigServerApplication.java
@@ -2,10 +2,8 @@ package com.shenjg.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
