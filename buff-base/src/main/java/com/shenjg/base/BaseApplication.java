26d27d40a402e07461bc9475db2953ca89706e3d
申建刚 <shenjiangang@thextrader.cn>
2019-01-22 17:44:52 +0800

    delete

--- buff-base/src/main/java/com/shenjg/base/BaseApplication.java
+++ buff-base/src/main/java/com/shenjg/base/BaseApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -25,9 +23,4 @@ public class BaseApplication {
 
     @Value("${server.port}")
     String port;
-
-    @RequestMapping("/hi")
-    public String home(@RequestParam String name) {
-        return "hi " + name + ",i am from port:" + port;
-    }
 }
