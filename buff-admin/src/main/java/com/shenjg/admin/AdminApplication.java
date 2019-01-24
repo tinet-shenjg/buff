44a1052389be44e0ed1e85b5215ce9dcd85331ee
申建刚 <shenjiangang@thextrader.cn>
2019-01-24 14:27:46 +0800

    delete

--- buff-admin/src/main/java/com/shenjg/admin/AdminApplication.java
+++ buff-admin/src/main/java/com/shenjg/admin/AdminApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
-@EnableEurekaClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan("com.shenjg")
 @EnableFeignClients("com.shenjg")
--- buff-base/src/main/java/com/shenjg/base/BaseApplication.java
+++ buff-base/src/main/java/com/shenjg/base/BaseApplication.java
@@ -4,13 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
 @RestController
 @EnableFeignClients("com.shenjg")
 @ComponentScan("com.shenjg")
