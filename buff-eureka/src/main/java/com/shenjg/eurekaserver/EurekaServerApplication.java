dee9fb16b6c3b49d4ccce145598e09cfdaf4570a
申建刚 <shenjiangang@thextrader.cn>
2019-01-24 14:18:44 +0800

    no message

--- buff-eureka/src/main/java/com/shenjg/eurekaserver/EurekaServerApplication.java
+++ buff-eureka/src/main/java/com/shenjg/eurekaserver/EurekaServerApplication.java
@@ -2,13 +2,20 @@ package com.shenjg.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run( EurekaServerApplication.class, args );
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EurekaServerApplication.class);
 	}
 }
